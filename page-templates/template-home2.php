8a62fd2b75052df1cb083479c2e66c2563e4252b
Christian Wahyunus Pangestu <christianwahyunuspangestu@yahoo.com>
2019-07-17 23:12:58 +0800

    F

--- page-templates/template-home2.php
+++ page-templates/template-home2.php
@@ -14,15 +14,15 @@
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<link rel="profile" href="http://gmpg.org/xfn/11"/>
-		<link rel="pingback" href="<?php echo get_site_url(); ?>/xmlrpc.php"/>
+		<link rel="pingback" href="http://delightfulyunion.online/xmlrpc.php"/>
 
 		<title>Yunus &amp; Delfina Wedding</title>
 		<link rel='dns-prefetch' href='//fonts.googleapis.com' />
 		<link rel='dns-prefetch' href='//s.w.org' />
-		<link rel="alternate" type="application/rss+xml" title="Yunus &amp; Delfina Wedding &raquo; Feed" href="<?php echo get_site_url(); ?>/feed/" />
-		<link rel="alternate" type="application/rss+xml" title="Yunus &amp; Delfina Wedding &raquo; Comments Feed" href="<?php echo get_site_url(); ?>/comments/feed/" />
+		<link rel="alternate" type="application/rss+xml" title="Yunus &amp; Delfina Wedding &raquo; Feed" href="http://delightfulyunion.online/feed/" />
+		<link rel="alternate" type="application/rss+xml" title="Yunus &amp; Delfina Wedding &raquo; Comments Feed" href="http://delightfulyunion.online/comments/feed/" />
 		<script type="text/javascript">
-			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/svg\/","svgExt":".svg","source":{"concatemoji":"<?php echo get_site_url(); ?>\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.1.1"}};
+			window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/11.2.0\/svg\/","svgExt":".svg","source":{"concatemoji":"http://delightfulyunion.online\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.1.1"}};
 			!function(a,b,c){function d(a,b){var c=String.fromCharCode;l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,a),0,0);var d=k.toDataURL();l.clearRect(0,0,k.width,k.height),l.fillText(c.apply(this,b),0,0);var e=k.toDataURL();return d===e}function e(a){var b;if(!l||!l.fillText)return!1;switch(l.textBaseline="top",l.font="600 32px Arial",a){case"flag":return!(b=d([55356,56826,55356,56819],[55356,56826,8203,55356,56819]))&&(b=d([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]),!b);case"emoji":return b=d([55358,56760,9792,65039],[55358,56760,8203,9792,65039]),!b}return!1}function f(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var g,h,i,j,k=b.createElement("canvas"),l=k.getContext&&k.getContext("2d");for(j=Array("flag","emoji"),c.supports={everything:!0,everythingExceptFlag:!0},i=0;i<j.length;i++)c.supports[j[i]]=e(j[i]),c.supports.everything=c.supports.everything&&c.supports[j[i]],"flag"!==j[i]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[j[i]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(h=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",h,!1),a.addEventListener("load",h,!1)):(a.attachEvent("onload",h),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),g=c.source||{},g.concatemoji?f(g.concatemoji):g.wpemoji&&g.twemoji&&(f(g.twemoji),f(g.wpemoji)))}(window,document,window._wpemojiSettings);
 		</script>
 		<style type="text/css">
@@ -39,26 +39,26 @@
 			padding: 0 !important;
 			}
 		</style>
-		<link rel='stylesheet' id='wp-block-library-css'  href='<?php echo get_site_url(); ?>/wp-includes/css/dist/block-library/style.min.css?ver=5.1.1' type='text/css' media='all' />
-		<link rel='stylesheet' id='contact-form-7-css'  href='<?php echo get_site_url(); ?>/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.1.1' type='text/css' media='all' />
-		<link rel="stylesheet" id="cf7cf-style-css" href='<?php echo get_site_url(); ?>/wp-content/plugins/cf7-conditional-fields/style.css?ver=1.4.1' type="text/css" media="all"/>
-		<link rel='stylesheet' id='bootstrap-css'  href='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/css/bootstrap.min.css?ver=5.1.1' type='text/css' media='all' />
-		<link rel='stylesheet' id='font-awesome-css'  href='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/css/font-awesome.min.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='wp-block-library-css'  href='http://delightfulyunion.online/wp-includes/css/dist/block-library/style.min.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='contact-form-7-css'  href='http://delightfulyunion.online/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel="stylesheet" id="cf7cf-style-css" href='http://delightfulyunion.online/wp-content/plugins/cf7-conditional-fields/style.css?ver=1.4.1' type="text/css" media="all"/>
+		<link rel='stylesheet' id='bootstrap-css'  href='http://delightfulyunion.online/wp-content/themes/shapely/assets/css/bootstrap.min.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='font-awesome-css'  href='http://delightfulyunion.online/wp-content/themes/shapely/assets/css/font-awesome.min.css?ver=5.1.1' type='text/css' media='all' />
 		<link rel='stylesheet' id='shapely-fonts-css'  href='//fonts.googleapis.com/css?family=Raleway%3A100%2C300%2C400%2C500%2C600%2C700&#038;ver=5.1.1' type='text/css' media='all' />
 		<link rel='stylesheet' id='flexslider-css'  href='https://cdnjs.cloudflare.com/ajax/libs/flexslider/2.7.2/flexslider.min.css' type='text/css' media='all' />
-		<link rel='stylesheet' id='owl.carousel-css'  href='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/owl-carousel/owl.carousel.min.css?ver=5.1.1' type='text/css' media='all' />
-		<link rel='stylesheet' id='owl.carousel.theme-css'  href='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/owl-carousel/owl.theme.default.css?ver=5.1.1' type='text/css' media='all' />
-		<link rel='stylesheet' id='shapely-style-css'  href='<?php echo get_site_url(); ?>/wp-content/themes/shapely/style.css?ver=5.1.1' type='text/css' media='all' />
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
-		<link rel='https://api.w.org/' href='<?php echo get_site_url(); ?>/wp-json/' />
-		<link rel="EditURI" type="application/rsd+xml" title="RSD" href="<?php echo get_site_url(); ?>/xmlrpc.php?rsd" />
-		<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="<?php echo get_site_url(); ?>/wp-includes/wlwmanifest.xml" /> 
+		<link rel='stylesheet' id='owl.carousel-css'  href='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/owl-carousel/owl.carousel.min.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='owl.carousel.theme-css'  href='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/owl-carousel/owl.theme.default.css?ver=5.1.1' type='text/css' media='all' />
+		<link rel='stylesheet' id='shapely-style-css'  href='http://delightfulyunion.online/wp-content/themes/shapely/style.css?ver=5.1.1' type='text/css' media='all' />
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-includes/js/jquery/jquery.js?ver=1.12.4'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+		<link rel='https://api.w.org/' href='http://delightfulyunion.online/wp-json/' />
+		<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://delightfulyunion.online/xmlrpc.php?rsd" />
+		<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://delightfulyunion.online/wp-includes/wlwmanifest.xml" /> 
 		<meta name="generator" content="WordPress 5.1.1" />
-		<link rel="canonical" href="<?php echo get_site_url(); ?>/" />
-		<link rel='shortlink' href='<?php echo get_site_url(); ?>/' />
-		<link rel="alternate" type="application/json+oembed" href="<?php echo get_site_url(); ?>/wp-json/oembed/1.0/embed?url=<?php echo get_site_url(); ?>%2F" />
-		<link rel="alternate" type="text/xml+oembed" href="<?php echo get_site_url(); ?>/wp-json/oembed/1.0/embed?url=<?php echo get_site_url(); ?>%2F&#038;format=xml" />
+		<link rel="canonical" href="http://delightfulyunion.online/" />
+		<link rel='shortlink' href='http://delightfulyunion.online/' />
+		<link rel="alternate" type="application/json+oembed" href="http://delightfulyunion.online/wp-json/oembed/1.0/embed?url=http://delightfulyunion.online%2F" />
+		<link rel="alternate" type="text/xml+oembed" href="http://delightfulyunion.online/wp-json/oembed/1.0/embed?url=http://delightfulyunion.online%2F&#038;format=xml" />
 		<style type="text/css"></style>
 	</head>
 
@@ -74,7 +74,7 @@
 						<div class="container nav-bar">
 							<div class="flex-row">
 								<div class="module left site-title-container">
-									<a href="<?php echo get_site_url(); ?>" class="custom-logo-link"><span class="site-title">Yunus &amp; Delfina Wedding</span></a>						</div>
+									<a href="http://delightfulyunion.online" class="custom-logo-link"><span class="site-title">Yunus &amp; Delfina Wedding</span></a>						</div>
 								<div class="module widget-handle mobile-toggle right visible-sm visible-xs">
 									<i class="fa fa-bars"></i>
 								</div>
@@ -107,7 +107,7 @@
 					<div id="main" class="" role="main">
 						<div id="sec1" class="widget shapely_home_parallax">
 							<section class="cover fullscreen image-bg">
-								<div class="parallax-window fullscreen" data-parallax="scroll" data-image-src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Hero-Parallax.jpg" data-ios-fix="true" data-over-scroll-fix="true" data-android-fix="true">
+								<div class="parallax-window fullscreen" data-parallax="scroll" data-image-src="http://delightfulyunion.online/wp-content/uploads/2019/03/Hero-Parallax.jpg" data-ios-fix="true" data-over-scroll-fix="true" data-android-fix="true">
 									<div class="align-transform">
 										<div class="row">
 											<div class="top-parallax-section">
@@ -195,14 +195,14 @@
 									<div class="row align-children">
 										<div class="col-md-11 col-sm-12">
 											<div class="owl-carousel owl-theme">
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Carousel-1.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Carousel-2.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Carousel-3.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Carousel-4.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2019/03/Carousel-5.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/Carousel-6.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/Carousel-7.jpg"/></div>
-												<div class="item"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/Carousel-8.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/2019/03/Carousel-1.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/2019/03/Carousel-2.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/2019/03/Carousel-3.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/2019/03/Carousel-4.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/2019/03/Carousel-5.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/Carousel-6.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/Carousel-7.jpg"/></div>
+												<div class="item"><img src="http://delightfulyunion.online/wp-content/uploads/Carousel-8.jpg"/></div>
 											</div>
 										</div>
 									</div>
@@ -266,28 +266,28 @@
 		</script>
 		<script type='text/javascript'>
 			/* <![CDATA[ */
-var wpcf7 = {"apiSettings":{"root":"<?php echo get_site_url(); ?>\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"}};
+var wpcf7 = {"apiSettings":{"root":"http://delightfulyunion.online\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"}};
 /* ]]> */
 		</script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.1.1'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/bootstrap.js?ver=20160115'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/skip-link-focus-fix.js?ver=20160115'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/flexslider.min.js?ver=20160222'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/parallax.min.js?ver=20160115'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/owl-carousel/owl.carousel.min.js?ver=20160115'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-includes/js/imagesloaded.min.js?ver=3.2.0'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=5.1.1'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/bootstrap.js?ver=20160115'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/skip-link-focus-fix.js?ver=20160115'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/flexslider.min.js?ver=20160222'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/parallax.min.js?ver=20160115'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/owl-carousel/owl.carousel.min.js?ver=20160115'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-includes/js/imagesloaded.min.js?ver=3.2.0'></script>
 		<script type='text/javascript'>
 			/* <![CDATA[ */
 var ShapelyAdminObject = {"sticky_header":"1"};
 /* ]]> */
 		</script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/shapely-scripts.js?ver=20180423'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-includes/js/wp-embed.min.js?ver=5.1.1'></script>
-		<script type='text/javascript' src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/assets/js/jquery.easing.1.3.js?ver=5.1.1'></script>
-		<script type="text/javascript" src='<?php echo get_site_url(); ?>/wp-content/plugins/cf7-conditional-fields/js/scripts.js?ver=1.4.1'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/shapely-scripts.js?ver=20180423'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-includes/js/wp-embed.min.js?ver=5.1.1'></script>
+		<script type='text/javascript' src='http://delightfulyunion.online/wp-content/themes/shapely/assets/js/jquery.easing.1.3.js?ver=5.1.1'></script>
+		<script type="text/javascript" src='http://delightfulyunion.online/wp-content/plugins/cf7-conditional-fields/js/scripts.js?ver=1.4.1'></script>
 		<script>
-			var urlRoot = "<?php echo get_site_url(); ?>";
+			var urlRoot = "http://delightfulyunion.online";
 		</script>
-		<script type="text/javascript" src='<?php echo get_site_url(); ?>/wp-content/themes/shapely/custom.js?ver=5.1.1'></script>
+		<script type="text/javascript" src='http://delightfulyunion.online/wp-content/themes/shapely/custom.js?ver=5.1.1'></script>
 	</body>
 </html>
