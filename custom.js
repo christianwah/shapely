d6c0031a11595cf2bc4b5001f3cc7e1d36d081eb
Christian Wahyunus Pangestu <christianwahyunuspangestu@yahoo.com>
2019-07-28 23:28:38 +0800

    Added if loader exst

--- custom.js
+++ custom.js
@@ -98,9 +98,11 @@
 
 		
 		
-		
-		document.getElementById("loader").style.display = "none";
-		document.getElementById("page").style.display = "block";
+		if (document.getElementById('loader') !== null)
+		{
+			document.getElementById("loader").style.display = "none";
+			document.getElementById("page").style.display = "block";
+		}
 		
 	})
 	
